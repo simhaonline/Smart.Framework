0d9df90a6321bc9e59416e9d22171e0a5070674e
ilies.ovidiu@googlemail.com <ilies.ovidiu@googlemail.com>
2016-06-27 14:43:11 +0300

    Task Engine

--- _scripts/task-engine/test-task.pl
+++ _scripts/task-engine/test-task.pl
@@ -8,29 +8,36 @@
 ### INFO: This Script can be customized as you need, this is just a sample of a TASK Script (Sample) ###
 ###
 
+
 ##### Test Sample Task #####
+
+
+######################################## PERL MODULES
 
 use strict;
 use warnings;
 use Cwd;
 use Time::HiRes;
-
 use Term::ANSIColor;
+
+######################################## TERM COLORS
 
 my $clr_error = ['bold bright_white on_red'];
 my $clr_warn = ['bold bright_white on_bright_red'];
 my $clr_notice = ['bold black on_bright_cyan'];
 my $clr_ok = ['bold bright_white on_green'];
 
+######################################## CHECK ARGUMENTS
+
 my $num_args = $#ARGV + 1;
 if($num_args != 1) {
-	print colored($clr_warn, "TASK.ERR : TEST.Task STOP: The ID parameter is missing (script must have 1 parameter) ...");
+	print colored($clr_error, "TASK.ERR : TEST.Task STOP: The ID parameter is missing (script must have 1 parameter) ...");
 	print "\n";
 	exit;
 }
 my $id = $ARGV[0];
 
-##### Task execution #####
+######################################## RUNTIME
 
 sleep 5; # this sample task execution is to wait 5 seconds (it can be changed as below ... see commented code ...)
 my $task_output = 200; # task result
@@ -39,6 +46,8 @@ my $task_output = 200; # task result
 #my $pass = '12345';
 #my $url = "http://your-url/smart-framework/admin.php?/page/tasks.test/id/".$id;
 #my $task_output = `curl -s -o /dev/null -w "%{http_code}" --get --connect-timeout 30 --max-time 600 --basic -u ${user}:${pass} --url "${url}"`;
+
+######################################## EVAL RESULT
 
 if($task_output != 200) {
 	if($task_output != 202) {
@@ -53,7 +62,7 @@ if($task_output != 200) {
 	print "\n";
 }
 
-##### End Task execution #####
+######################################## FINALIZE
 
 exit 0
 
