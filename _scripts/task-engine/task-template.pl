baab0e0fa1e883f43cc254e549e5bcf287f73142
ilies.ovidiu@googlemail.com <ilies.ovidiu@googlemail.com>
2016-06-27 17:09:50 +0300

    TMP svn.314

--- _scripts/task-engine/task-template.pl
+++ _scripts/task-engine/task-template.pl
@@ -70,20 +70,27 @@ my $url = str_single_quotes_escapeshellarg($inisett{'TaskURLAddr'});
 
 ######################################## RUNTIME
 
-sleep 5; # this sample task execution is to wait 5 seconds (it can be changed as below ... see commented code ...)
-
+my $is_url = str_begins_with($url, 'http://') || str_begins_with($url, 'https://');
 my $task_output = 777; # task result
 
 if($url ne "") {
-	my $auth = "";
-	if($user ne "") {
-		if($pass ne "") { # use auth: user and pass
-			$auth = " --basic -u '".str_single_quotes_escapeshellarg($user).":".str_single_quotes_escapeshellarg($pass)."'";
-		} else { # use auth: only user
-			$auth = " --basic -u '".str_single_quotes_escapeshellarg($user)."'";
+	$url = $url = str_single_quotes_escapeshellarg($url.$id);
+	if($is_url eq "OK") {
+		my $auth = "";
+		if($user ne "") {
+			if($pass ne "") { # use auth: user and pass
+				$auth = " --basic -u '".str_single_quotes_escapeshellarg($user).":".str_single_quotes_escapeshellarg($pass)."'";
+			} else { # use auth: only user
+				$auth = " --basic -u '".str_single_quotes_escapeshellarg($user)."'";
+			}
 		}
+		$task_output = `curl -s -o /dev/null -w '%{http_code}' --get --connect-timeout 30 --max-time 600${auth} --url '${url}'`; # use auth with user and pass
+	} else {
+		$task_output = 404; # URL Invalid or Not Found !!!
 	}
-	$task_output = `curl -s -o /dev/null -w '%{http_code}' --get --connect-timeout 30 --max-time 600${auth} --url '${url}'`; # use auth with user and pass
+} else {
+	$task_output = 888;
+	sleep 5; # this sample task execution is to wait 5 seconds (it can be changed as below ... see commented code ...)
 }
 
 ######################################## EVAL RESULT
