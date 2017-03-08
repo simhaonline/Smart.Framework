a76ccbae3a874df884ef5d7198627f0022890d49
unix-world <iradu@unix-world.org>
2017-03-08 19:37:32 +0200

    TMP svn.853

--- _scripts/smart-framework-js.sh
+++ _scripts/smart-framework-js.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-# Combine all Smart.Framework JS scripts from lib/js/framework/*.js into one JS file: smart-framework.js
+# Combine all required Smart.Framework JS scripts from lib/js/framework/src/*.js into one JS file: lib/js/framework/smart-framework.js
 
-THE_DTIME=`date +%Y%m%d.%H%M%S`
+THE_SVN_REVISION=`svn info --show-item revision`
+THE_FILE=../lib/js/framework/smart-framework.js
 
-echo "// JS Combined: smart-framework.js from lib/js/framework/ @ r.${THE_DTIME}" > ./smart-framework.js
-cat ../lib/js/framework/browser_check.js >> ./smart-framework.js
-cat ../lib/js/framework/core_utils.js >> ./smart-framework.js
-cat ../lib/js/framework/arch_utils.js >> ./smart-framework.js
-cat ../lib/js/framework/crypt_utils.js >> ./smart-framework.js
-cat ../lib/js/framework/ifmodalbox.js >> ./smart-framework.js
-cat ../lib/js/framework/browser_utils.js >> ./smart-framework.js
-cat ../lib/js/framework/ifmodalbox_scanner.js >> ./smart-framework.js
-cat ../lib/js/framework/validate_input.js >> ./smart-framework.js
-echo "" >> ./smart-framework.js
-echo "// #END# JS Combined: smart-framework.js from lib/js/framework/" >> ./smart-framework.js
+echo "// JS Combined: smart-framework.js from lib/js/framework/src/*.js @ rev: svn.${THE_SVN_REVISION}" > ${THE_FILE}
+cat ../lib/js/framework/src/browser_check.js >> ${THE_FILE}
+cat ../lib/js/framework/src/core_utils.js >> ${THE_FILE}
+cat ../lib/js/framework/src/arch_utils.js >> ${THE_FILE}
+cat ../lib/js/framework/src/crypt_utils.js >> ${THE_FILE}
+cat ../lib/js/framework/src/ifmodalbox.js >> ${THE_FILE}
+cat ../lib/js/framework/src/browser_utils.js >> ${THE_FILE}
+cat ../lib/js/framework/src/ifmodalbox_scanner.js >> ${THE_FILE}
+cat ../lib/js/framework/src/validate_input.js >> ${THE_FILE}
+echo "" >> ${THE_FILE}
+echo "// #END# JS Combined: smart-framework.js from lib/js/framework/" >> ${THE_FILE}
 
 # END
