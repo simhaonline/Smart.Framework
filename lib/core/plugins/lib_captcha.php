0d005614a096293e7cf63de9924f4c52a58ec831
unix-world <iradu@unix-world.org>
2017-03-16 13:46:24 +0200

    TMP svn.882

--- lib/core/plugins/lib_captcha.php
+++ lib/core/plugins/lib_captcha.php
@@ -15,10 +15,13 @@ if((!defined('SMART_FRAMEWORK_VERSION')) || ((string)SMART_FRAMEWORK_VERSION != 
 // DEPENDS:
 //	* Smart::
 //	* SmartUtils::
+//	* SmartFileSysUtils::
 //	* SmartTextTranslations::
 // DEPENDS-EXT: PHP GD w. *optional TTF support
 // REQUIRED CSS:
 //	* captcha.css
+// REQUIRED TEMPLATES:
+//	* captcha-form.inc.htm
 //======================================================
 
 
@@ -53,7 +56,7 @@ if((!defined('SMART_FRAMEWORK_VERSION')) || ((string)SMART_FRAMEWORK_VERSION != 
  *
  * @access 		PUBLIC
  * @depends 	extensions: PHP XML ; classes: Smart
- * @version 	v.160927
+ * @version 	v.170316
  * @package 	Components:Captcha
  *
  */
@@ -81,14 +84,14 @@ public static function captcha_image($y_form_name, $y_store, $y_mode='hashed', $
 	$captcha->charxvar = Smart::format_number_int($y_char_xvary,'+');
 	$captcha->charyvar = Smart::format_number_int($y_char_yvary,'+');
 	$captcha->noise = Smart::format_number_int($y_noise,'+');
-	if(is_array($y_char_colors)) {
+	if(Smart::array_size($y_char_colors) > 0) {
 		$captcha->colors_chars = (array)$y_char_colors;
 	} //end if
-	if(is_array($y_noise_colors)) {
+	if(Smart::array_size($y_noise_colors) > 0) {
 		$captcha->colors_noise = (array)$y_noise_colors;
 	} //end if
 	//--
-	return $captcha->draw_image((string)$y_form_name);
+	return (string) $captcha->draw_image((string)$y_form_name);
 	//--
 } //END FUNCTION
 //================================================================
@@ -328,31 +331,32 @@ public static function cookiename($y_form_name) {
 final class SmartCaptchaImageDraw {
 
 	// ->
-	// v.161005
+	// v.170316
 
 
 //================================================================
 //--
 public $store = 'cookie';		// captcha mode: 'cookie' | 'session'
-public $format = 'jpg';			// default format: jpg/png/gif
-public $mode = 'dotted'; 		// captcha noise style: dotted, hashed
-public $noise = 88; 			// captcha noise level 10..500
+public $format = 'jpg';			// default format: png | gif | jpg
+public $mode = 'dotted'; 		// captcha noise style: dotted | hashed
+public $noise = 88; 			// captcha noise level 10..1000
 //--
-public $width = 50;				// image default width
-public $height = 25;			// image default height
+public $width = 80;				// image default width:  80..320
+public $height = 40;			// image default height: 40..160
 public $quality = 90; 			// image quality 1..100 (just for jpeg)
 //--
 public $pool = '01234567890'; 	// captcha charset: 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-public $chars = 5;				// captcha number of characters
+public $chars = 5;				// captcha number of characters: 3..5
 //--
-public $charfont = 5; 			// character's font (1..5 for built-in gd font ; path/to/font.gdf ; path/to/font.ttf)
-public $charttfsize = 24; 		// ttf font size (just for ttf fonts)
 public $charspace = 8; 			// char space
 public $charxvar = 7; 			// the X start limit > 2
 public $charyvar = 8; 			// char Y start limit > 2
 //--
-public $colors_chars = array(0x111111, 0x333333, 0x778899, 0x666699, 0x003366, 0x669966, 0x006600, 0xFF3300); // color palette characters
-public $colors_noise = array(0x888888, 0x999999, 0xAAAAAA, 0xBBBBBB, 0xCCCCCC, 0xDDDDDD, 0xEEEEEE, 0x8080C0); // color palette noise
+public $colors_chars = [0x111111, 0x333333, 0x778899, 0x666699, 0x003366, 0x669966, 0x006600, 0xFF3300]; // color palette characters
+public $colors_noise = [0x888888, 0x999999, 0xAAAAAA, 0xBBBBBB, 0xCCCCCC, 0xDDDDDD, 0xEEEEEE, 0x8080C0]; // color palette noise
+//--
+public $charfont = 5; 			// character's font (1..5 for built-in gd font ; path/to/font.gdf ; path/to/font.ttf)
+public $charttfsize = 24; 		// ttf font size (just for ttf fonts)
 //--
 //================================================================
 
@@ -376,6 +380,73 @@ public function draw_image($y_form) {
 	//--
 
 	//--
+	$this->noise = (int) $this->noise;
+	if($this->noise < 10) {
+		$this->noise = 10;
+	} elseif($this->noise > 1000) {
+		$this->noise = 1000;
+	} //end if
+	//--
+	$this->width = (int) $this->width;
+	if($this->width < 80) {
+		$this->width = 80;
+	} elseif($this->width > 320) {
+		$this->width = 320;
+	} //end if
+	//--
+	$this->height = (int) $this->height;
+	if($this->height < 40) {
+		$this->height = 40;
+	} elseif($this->height > 160) {
+		$this->height = 160;
+	} //end if
+	//--
+	$this->quality = (int) $this->quality;
+	if($this->quality < 50) {
+		$this->quality = 50;
+	} elseif($this->quality > 100) {
+		$this->quality = 100;
+	} //end if else
+	//--
+	$this->pool = (string) trim((string)$this->pool);
+	if((string)$this->pool == '') {
+		$this->pool = '01234567890';
+	} //end if
+	//--
+	$this->chars = (int) $this->chars;
+	if($this->chars < 3) {
+		$this->chars = 3;
+	} elseif($this->chars > 10) {
+		$this->chars = 10;
+	} //end if else
+	//--
+	$this->charspace = (int) $this->charspace;
+	if($this->charspace < 1) {
+		$this->charspace = 1;
+	} elseif($this->charspace > 100) {
+		$this->charspace = 100;
+	} //end if
+	//--
+	$this->charxvar = (int) $this->charxvar;
+	if($this->charxvar < 0) {
+		$this->charxvar = 0;
+	} elseif($this->charxvar > 100) {
+		$this->charxvar = 100;
+	} //end if else
+	//--
+	$this->charyvar = (int) $this->charyvar;
+	if($this->charyvar < 0) {
+		$this->charyvar = 0;
+	} elseif($this->charyvar > 100) {
+		$this->charyvar = 100;
+	} //end if else
+	//--
+	$this->colors_chars = (array) $this->colors_chars;
+	//--
+	$this->colors_noise = (array) $this->colors_noise;
+	//--
+
+	//--
 	$out = '';
 	//--
 	ob_start();
@@ -385,10 +456,9 @@ public function draw_image($y_form) {
 	} else { // 'dotted'
 		$captcha_arr = (array) $this->generate_captcha_dotted();
 	} //end if else
-	//--
 	$captcha_image = $captcha_arr['rawimage'];
 	$captcha_word = $captcha_arr['word'];
-	$captcha_arr = array();
+	unset($captcha_arr);
 	//--
 	$err = ob_get_contents();
 	ob_end_clean();
@@ -397,22 +467,27 @@ public function draw_image($y_form) {
 		Smart::log_warning('#Captcha / Draw Image ['.$intext.'] Errors/Output: '.$err);
 	} //end if
 	//--
+	if(!is_resource($captcha_image)) {
+		Smart::log_warning('#Captcha / Draw Image :: Invalid Resource');
+		return '';
+	} //end if
+	//--
 	ob_start();
 	//-
 	switch(@strtolower($this->format)) {
-		case "png":
+		case 'png':
 			//header: "Content-type: image/png"
-			imagepng($captcha_image);
+			@imagepng($captcha_image);
 			break;
-		case "gif":
+		case 'gif':
 			//header: "Content-type: image/gif"
-			imagegif($captcha_image);
+			@imagegif($captcha_image);
 			break;
-		case "jpg":
-		case "jpeg":
+		case 'jpg':
+		case 'jpeg':
 		default:
 			//header: "Content-type: image/jpeg"
-			imagejpeg($captcha_image, '', $this->quality);
+			@imagejpeg($captcha_image, '', $this->quality);
 	} //end switch
 	//-
 	$out = ob_get_contents();
@@ -435,30 +510,9 @@ public function draw_image($y_form) {
 	//--
 
 	//--
-	return $out;
+	return (string) $out;
 	//--
 
-} //END FUNCTION
-//================================================================
-
-
-//================================================================
-private function generate_word() {
-	//--
-	$pool = (string) $this->pool;
-	$len = (int) strlen($pool) - 1;
-	if($len <= 0) {
-		$len = 1;
-	} //end if
-	//--
-	$str = '';
-	//--
-	for($i = 0; $i<$this->chars; $i++) {
-		$str .= substr($pool, Smart::random_number(0, (int)$len), 1);
-	} //end for
-	//--
-	return $str;
-	//--
 } //END FUNCTION
 //================================================================
 
@@ -518,103 +572,136 @@ private function generate_noise_color() {
 
 
 //================================================================
-private function generate_captcha_dotted() {
-
-	//-- inits
-	$captcha_word = $this->generate_word();
+private function generate_word() {
 	//--
-
-	//--
-
-	$vfy_arr = array();
-	//-- generate the random number
-	for($i=0; $i<$this->chars; $i++) {
-		$char = $this->generate_word();
-		$vfy_arr[] = $char;
-	} //end for
-	//--
-
-	//-- create image
-	if(function_exists('imagecreatetruecolor')) {
-		$captcha_image = @imagecreatetruecolor($this->width, $this->height);
-	} else {
-		$captcha_image = @imagecreate($this->width, $this->height);
-	} //end if else
-	//-
-	@imagefill($captcha_image, 0, 0, 0xDDDDDD);
-	//--
-
-	//-- add horiz lines
-	$first_x = 2;
-	$max_lines = ceil($this->width / 7.5);
-	for($i=0; $i<$max_lines; $i++) {
-		@imageline ($captcha_image, $first_x, 2, $first_x, ($this->height-2), 0xFFFFFF);
-		$first_x += 7.5;
-	} //end for
-	//--
-
-	//-- add vert lines
-	$first_y = 2;
-	$max_lines = ceil($this->width / 7);
-	for($i=0; $i<$max_lines; $i++) {
-		@imageline ($captcha_image, 2, $first_y, ($this->width-2), $first_y, 0xFFFFFF);
-		$first_y += 7;
-	} //end for
-	//--
-
-	//-- add strings
-	$use_ttf_font = false;
-	if(is_int($this->charfont)) {
-		$font = $this->charfont ;
-	} else {
-		if(is_file($this->charfont)) {
-			if(function_exists('imagettftext') AND (substr($this->charfont, -4, 4) == '.ttf')) {
-				$font = (string) $this->charfont;
-				$use_ttf_font = true;
-			} else { // gdf font
-				$font = @imageloadfont($this->charfont);
-				if ($font == false) {
-					$font = 5;
-				} //end if
-			} //end if
-		} else {
-			$font = 5 ; // on error
-		} //end if else
+	$pool = (string) $this->pool;
+	$len = (int) strlen($pool) - 1;
+	if($len <= 0) {
+		$len = 1;
 	} //end if
+	//--
+	$str = '';
+	//--
+	for($i = 0; $i<$this->chars; $i++) {
+		$str .= substr($pool, Smart::random_number(0, (int)$len), 1);
+	} //end for
+	//--
+	return (string) $str;
+	//--
+} //END FUNCTION
+//================================================================
+
+
+//================================================================
+private function img_draw_text($im, $word) {
+	//--
+	$use_ttf_font = false;
+	if(is_int($this->charfont) AND ($this->charfont > 0)) {
+		$font = (int) $this->charfont;
+	} elseif(((string)$this->charfont != '') AND (SmartFileSysUtils::check_file_or_dir_name($this->charfont)) AND (is_file($this->charfont))) {
+		if(function_exists('imagettftext') AND (substr($this->charfont, -4, 4) == '.ttf')) {
+			$font = (string) $this->charfont;
+			$use_ttf_font = true;
+		} else { // gdf font
+			$font = @imageloadfont($this->charfont);
+			if($font === false) {
+				$font = 5; // on error
+			} //end if
+		} //end if else
+	} else {
+		$font = 5 ; // on error
+	} //end if else
 	//--
 	$first_x = Smart::random_number(5, $this->charxvar);
 	//--
-	for($i=0; $i<strlen($captcha_word); $i++) {
+	for($i=0; $i < strlen($word); $i++) {
 		//--
-		$w = substr($captcha_word, $i, 1);
+		$w = substr($word, $i, 1);
 		$c = $this->generate_color();
 		//--
+		$sign = 1;
+		if(Smart::random_number(0, 1) != 0) {
+			$sign = -1;
+		} //end if
+		//--
 		if($use_ttf_font != true) { // GDF font
-			$y = Smart::random_number(2, $this->charyvar);
-			@imagestring($captcha_image, $font, $first_x, $y, $w, $c);
+			$y = ($this->height / 2) + ($sign * Smart::random_number(0, $this->charyvar));
+			@imagestring($im, (int)$font, (int)$first_x, (int)$y, (string)$w, $c);
 		} else { // TTF font
+			$y = ($this->height / 2) + ($this->charttfsize / 2) + ($sign * Smart::random_number(0, $this->charyvar));
 			$angle = Smart::random_number(0, 20);
-			$y = ($this->height / 2) + ($this->charttfsize / 2) - Smart::random_number(2, $this->charyvar);
-			@imagettftext($captcha_image, $this->charttfsize, $angle, $first_x, $y, $c, $font, $w);
+			@imagettftext($im, $this->charttfsize, (int)$angle, (int)$first_x, (int)$y, $c, (string)$font, (string)$w);
 		} //end if else
 		//--
 		$first_x += (int) $this->charspace + Smart::random_number(1, 15);
 		//--
 	} //end for
 	//--
+} // END FUNCTION
+//================================================================
+
+
+//================================================================
+private function generate_captcha_dotted() {
+
+	//-- inits
+	$word = (string) $this->generate_word();
+	//--
+
+	//-- create image
+	if(function_exists('imagecreatetruecolor')) {
+		$im = @imagecreatetruecolor($this->width, $this->height);
+	} else {
+		$im = @imagecreate($this->width, $this->height);
+	} //end if else
+	//-
+	@imagefill($im, 0, 0, 0xDDDDDD);
+	//--
+
+	//-- add horiz lines
+	$margin = 1;
+	$first_x = $margin;
+	$factor = 7;
+	$max_lines = ceil($this->width / $factor);
+	for($i=0; $i<$max_lines; $i++) {
+		if($first_x > ($this->width - $margin)) {
+			break;
+		} //end if
+		@imageline ($im, $first_x, 2, $first_x, ($this->height-2), 0xFFFFFF);
+		$first_x += ceil($factor);
+	} //end for
+	//--
+
+	//-- add vert lines
+	$margin = 1;
+	$first_y = $margin;
+	$factor = 7;
+	$max_lines = ceil($this->height / $factor);
+	for($i=0; $i<$max_lines; $i++) {
+		if($first_y > ($this->height - $margin)) {
+			break;
+		} //end if
+		@imageline ($im, 2, $first_y, ($this->width-2), $first_y, 0xFFFFFF);
+		$first_y += ceil($factor);
+	} //end for
+	//--
+
+	//-- add text
+	$this->img_draw_text($im, $word);
+	//--
 
 	//-- add noise
 	for($i=0; $i<$this->noise; $i++){
 		$noise_color = $this->generate_noise_color();
-		@imagesetpixel($captcha_image, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
-		@imagesetpixel($captcha_image, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
-		@imagesetpixel($captcha_image, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
-		@imagesetpixel($captcha_image, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
+		@imagesetpixel($im, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
+		@imagesetpixel($im, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
+		@imagesetpixel($im, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
+		@imagesetpixel($im, Smart::random_number(2,$this->width-2), Smart::random_number(2,$this->height-2), $noise_color);
 	} //end for
 	//--
 
 	//--
-	return array('word' => $captcha_word, 'rawimage' => $captcha_image);
+	return array('word' => (string)$word, 'rawimage' => $im);
 	//--
 
 } //END FUNCTION
@@ -624,56 +711,30 @@ private function generate_captcha_dotted() {
 //================================================================
 private function generate_captcha_hashed() {
 
-	// v.130328
-	// based on CodeIgniter
+	// v.170316
+	// portions of this code is based on CodeIgniter
 
 	//--
-	$img_width = (int) 0 + $this->width;
-	$img_height = (int) 0 + $this->height;
-	//--
-
-	//--
-	$use_ttf_font = false;
-	if(is_int($this->charfont)) {
-		$font = $this->charfont ;
-	} else {
-		if(is_file($this->charfont)) {
-			if(function_exists('imagettftext') AND (substr($this->charfont, -4, 4) == '.ttf')) {
-				$font = (string) $this->charfont;
-				$use_ttf_font = true;
-			} else { // gdf font
-				$font = @imageloadfont($this->charfont);
-				if ($font == false) {
-					$font = 5;
-				} //end if
-			} //end if else
-		} else {
-			$font = 5 ; // on error
-		} //end if else
-	} //end if
-	//--
-
-	//--
-	$word = $this->generate_word();
+	$word = (string) $this->generate_word();
 	//--
 
 	//--
 	$length	= strlen($word);
 	$angle	= ($length >= 6) ? Smart::random_number(-($length-6), ($length-6)) : 0;
 	$x_axis	= Smart::random_number(6, (360 / $length)-16);
-	$y_axis = ($angle >= 0 ) ? Smart::random_number($img_height, $img_width) : Smart::random_number(6, $img_height);
+	$y_axis = ($angle >= 0 ) ? Smart::random_number($this->height, $this->width) : Smart::random_number(6, $this->height);
 	//--
 
 	//--
 	if(function_exists('imagecreatetruecolor')) {
-		$im = @imagecreatetruecolor($img_width, $img_height);
+		$im = @imagecreatetruecolor($this->width, $this->height);
 	} else {
-		$im = @imagecreate($img_width, $img_height);
+		$im = @imagecreate($this->width, $this->height);
 	} //end if else
 	//--
 
 	//--
-	@imagefilledrectangle($im, 0, 0, $img_width, $img_height, 0xFFFFFF);
+	@imagefilledrectangle($im, 0, 0, $this->width, $this->height, 0xFFFFFF);
 	//--
 
 	//--
@@ -710,42 +771,25 @@ private function generate_captcha_hashed() {
 	//--
 
 	//--
-	$first_x = Smart::random_number(5, $this->charxvar);
-	//--
-	for($i=0; $i < strlen($word); $i++) {
-		//--
-		$w = substr($word, $i, 1);
-		$c = $this->generate_color();
-		//--
-		if($use_ttf_font != true) { // GDF font
-			$y = Smart::random_number(2, $this->charyvar);
-			@imagestring($im, $font, $first_x, $y, $w, $c);
-		} else { // TTF font
-			$y = ($img_height / 2) + ($this->charttfsize / 2) - Smart::random_number(2, $this->charyvar);
-			$angle = Smart::random_number(0, 20);
-			@imagettftext($im, $this->charttfsize, $angle, $first_x, $y, $c, $font, $w);
-		} //end if else
-		//--
-		$first_x += (int) $this->charspace + Smart::random_number(1, 15);
-		//--
-	} //end for
+	$this->img_draw_text($im, $word);
 	//--
 
 	//--
-	return array('word' => $word, 'rawimage' => $im);
+	return array('word' => (string)$word, 'rawimage' => $im);
 	//--
 
 } //END FUNCTION
 //================================================================
 
+
 // Sample
 /*
 $captcha = new SmartCaptchaImageDraw();
-$captcha->store = 'session';
-$captcha->format='gif';
+$captcha->store = 'cookie';
+$captcha->format='png';
 $captcha->width = 100;
 $captcha->height = 50;
-$captcha->noise = 500;
+$captcha->noise = 300;
 $captcha->chars = 5;
 $captcha->charfont = 'lib/core/plugins/fonts/adventure.gdf';
 $captcha->charspace = 18;
